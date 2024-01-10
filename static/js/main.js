0f1cd9e04d4e4c0c5ca3ba3cd00efb1c95a5fcd7
Ivan Sirant <ioannvs10sirant@gmail.com>
2024-01-10 23:48:31 +0500

    Ver. 0.1.4 Темная тема

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,10 @@
+import setDarkThemeToggler from "./dark-theme.js";
+
 window.addEventListener('load', setStartInfo);
 
 function setStartInfo() {
+    setDarkThemeToggler();
+
     document.getElementById('changelog-egg')
         .addEventListener('click', function () {window.location.href = '/study/changelog';});
 }
