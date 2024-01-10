78cde739ee81f65b86bce7b97a006b4cf7a41b7b
Ivan Sirant <ioannvs10sirant@gmail.com>
2024-01-10 23:02:23 +0500

    Ver. 0.1.2 Исправлен путь к config

--- set.sh
+++ set.sh
@@ -2,6 +2,6 @@
 
 cp ../pyStudy.service ../../../../../etc/systemd/system
 
-sed -i "s/^current_version:.*/current_version: 0.0.1/" config.yaml
+sed -i "s/^current_version:.*/current_version: 0.0.1/" ../config.yaml
 
 systemctl daemon-reload
