66a66c48f0e519ee1b7d972205eb5ad795d66b6f
time name <birukovvasilij10@gmail.com>
2024-01-12 23:39:02 +0500

    Ver 0.1.11 деление на классы

--- a_parser_for_wikipedia.py
+++ a_parser_for_wikipedia.py
@@ -2,24 +2,32 @@ import textwrap
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://ru.wikipedia.org/wiki/Тобольск"
-try:
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text, "html.parser")
+class Reading():
+     def __init__(self, url):
+         self.url = url
+     def reading(self):
+         all_info = []
+         res = requests.get(self.url)
+         soup = BeautifulSoup(res.text, "lxml")
 
-    body = soup.find("body")
-    article_name = body.find("h1").text
-    article_content = body.find("div", class_="mw-content-ltr mw-parser-output")
+         body = soup.find("body")
+         article_name = body.find("h1").text
+         article_content = body.find("div", class_="mw-content-ltr mw-parser-output")
 
-    print(f"\n\nНазвание статьи : {article_name}")
-    tags = article_content.find_all("p")
-    for t in tags:
-        info = t.text
-        print("\n\n" + textwrap.fill(info.replace(" ", ""), width=100))
+         tags = article_content.find_all("p")
+         for t in tags:
+             info = t.text
+             all_info.append(info)
+         return [article_name, all_info]
 
-except requests.exceptions.InvalidSchema:
-    print("Необходимо ввести адрес URL ссылки!")
-except AttributeError:
-    print("\n\nДанная URL ссылка не относиться к википедии")
-except FileNotFoundError:
-    print("Данный файл не существует")
+class write_file():
+    def __init__(self, url, name_file):
+        self.url = url
+        self.name_file = name_file
+
+    def parser(self):
+        reader = Reading(self.url)
+        with open(self.name_file, "w", encoding="utf-8", errors='ignore') as file:
+                for line in reader.reading():
+                    file.write("\n\n" + textwrap.fill(line).replace(" ", ""), width = 100)
+
